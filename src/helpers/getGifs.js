efa72c7148878d08eff635e81dc8f91a9c425487
Moises Barillas <moisesrocha717@gmail.com>
2020-12-19 16:52:05 -0600

    mod

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGif = async (category) => {
-	const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(
+	const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
 		category
 	)}&limit=10&api_key=lwukRJhjm6MjqVntNrmb7XZAFvP4L1im`;
 
